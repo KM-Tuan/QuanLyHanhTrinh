679d7711f03fe064bd3ad2c09bd69a0840837e27
KM-Tuan <kieuminhtuan@gmail.com>
2025-07-24 22:31:54 +0700

    The first step

---
+++ SpringJourneyApp/src/main/java/com/kmt/configs/WebAppContextConfigs.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.kmt.configs;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author kieum
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {
+    "com.kmt.controllers"
+})
+public class WebAppContextConfigs implements WebMvcConfigurer {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+}
